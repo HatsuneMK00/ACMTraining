7fc8b164223215f43ac60977bdccc11ba1176664
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-02-11 14:41:09 +0800

    update: Problem17 of leetcode

---
+++ src/xyz/leetcode/Problem17.java
@@ -0,0 +1,45 @@
+package xyz.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Problem17 {
+    static HashMap<Character, List<String>> keyMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        Problem17 problem17 = new Problem17();
+        System.out.println(problem17.letterCombinations("23"));
+    }
+
+    static {
+        keyMap.put('2', List.of("a", "b", "c"));
+        keyMap.put('3', List.of("d", "e", "f"));
+        keyMap.put('4', List.of("g", "h", "i"));
+        keyMap.put('5', List.of("j", "k", "l"));
+        keyMap.put('6', List.of("m", "n", "o"));
+        keyMap.put('7', List.of("p", "q", "r", "s"));
+        keyMap.put('8', List.of("t", "u", "v"));
+        keyMap.put('9', List.of("w", "x", "y", "z"));
+    }
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) {
+            return new ArrayList<String>();
+        }
+        if (digits.length() == 1) {
+            return new ArrayList<>(keyMap.get(digits.charAt(0)));
+        }
+        int length = digits.length();
+        List<String> list = letterCombinations(digits.substring(0, length - 1));
+        List<String> newList = new ArrayList<>();
+
+        for (String item : list) {
+            for (String key: keyMap.get(digits.charAt(length - 1))) {
+                newList.add(item.concat(key));
+            }
+        }
+
+        return newList;
+    }
+}
