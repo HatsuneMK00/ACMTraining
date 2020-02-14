7b087b00f3587e766ab8a66203073a4bef288f44
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-02-14 15:28:21 +0800

    update: Problem38

---
+++ src/xyz/leetcode/Problem38.java
@@ -0,0 +1,28 @@
+package xyz.leetcode;
+
+public class Problem38 {
+    public static void main(String[] args) {
+        Problem38 problem38 = new Problem38();
+        System.out.println(problem38.countAndSay(4));
+    }
+
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        String str = countAndSay(n - 1);
+        str = str.concat(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i - 1) == str.charAt(i)) {
+                count++;
+            } else {
+                stringBuilder.append(Integer.toString(count));
+                stringBuilder.append(str.charAt(i - 1));
+                count = 1;
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
