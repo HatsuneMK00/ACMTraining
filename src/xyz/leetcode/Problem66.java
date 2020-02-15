ba0820d7603f7e0a868c581dfd19ea861e5f92c7
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-02-15 14:26:08 +0800

    update: Problem66

---
+++ src/xyz/leetcode/Problem66.java
@@ -0,0 +1,31 @@
+package xyz.leetcode;
+
+import java.util.Arrays;
+
+//TODO 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一
+public class Problem66 {
+    //    除了整数0外，不会以0开头
+    public int[] plusOne(int[] digits) {
+        int plus = 0;
+        int i = 1;
+        int length = digits.length;
+        digits[length - 1]++;
+        if (digits[length - 1] >= 10) {
+            plus = 1;
+            digits[length - 1] -= 10;
+        }
+        while (plus > 0 && i < length) {
+            digits[length - 1 - i]++;
+            if (digits[length - 1 - i] >= 10) digits[length - 1 - i] -= 10;
+            else plus = 0;
+            i++;
+        }
+        if (plus == 1) {
+            int[] newDigits = new int[length + 1];
+            newDigits[0] = 1;
+            System.arraycopy(digits, 0, newDigits, 1, length);
+            return newDigits;
+        }
+        return digits;
+    }
+}
