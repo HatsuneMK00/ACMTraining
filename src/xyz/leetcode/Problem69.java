f73a2c57403d5469ae7fc8983ae514405a98d865
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-02-16 14:22:03 +0800

    update: Problem69

---
+++ src/xyz/leetcode/Problem69.java
@@ -0,0 +1,7 @@
+package xyz.leetcode;
+
+public class Problem69 {
+    public int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
---
+++ src/xyz/leetcode/Problem75.java
@@ -0,0 +1,7 @@
+package xyz.leetcode;
+
+public class Problem75 {
+    public void sortColors(int[] nums) {
+
+    }
+}
