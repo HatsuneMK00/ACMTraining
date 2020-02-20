e35fd0193587221dc2452c90a26d52bd4d0d8e8c
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-02-20 14:41:00 +0800

    update: Problem125 of leetcode

---
+++ src/xyz/leetcode/Problem125.java
@@ -0,0 +1,29 @@
+package xyz.leetcode;
+
+public class Problem125 {
+    public boolean isPalindrome(String s) {
+        if (s.length() == 0) {
+            return true;
+        }
+        int start = 0;
+        int end = s.length() - 1;
+        boolean isPalindrome = true;
+        while (start <= end) {
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        return isPalindrome;
+    }
+}
