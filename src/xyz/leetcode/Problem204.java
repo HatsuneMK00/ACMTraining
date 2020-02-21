34a9779d5d82e0eb8c341f24717c7862cce43199
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-02-21 14:44:53 +0800

    update: Problem204 of leetcode

---
+++ src/xyz/leetcode/Problem204.java
@@ -0,0 +1,25 @@
+package xyz.leetcode;
+
+public class Problem204 {
+    public int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+//    TOOL 判断一个数是否是质数
+    public boolean isPrime(int n) {
+        int i = 2;
+        while (i * i <= n) {
+            if (n % i == 0) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
