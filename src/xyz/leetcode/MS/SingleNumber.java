42d3673b4910fed3100e688002b3e77b215747b1
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-04-28 18:23:41 +0800

    accepted

---
+++ src/xyz/leetcode/MS/SingleNumber.java
@@ -0,0 +1,18 @@
+package xyz.leetcode.MS;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+        return set.iterator().next();
+    }
+}
